07c4850bc3b0c7a490432e12515dc7f9506e2923
FaustVX <jodu035@gmail.com>
2022-02-26 21:29:42 +0100

    Increased Redraw Console Speed

--- NoiseWorldGen.CLI/Program.cs
+++ NoiseWorldGen.CLI/Program.cs
@@ -6,23 +6,24 @@ Console.CursorVisible = false;
 var world = new World(Environment.TickCount, Console.WindowHeight - 3);
 var width = Console.WindowWidth / 2 - 1;
 var posX = width;
-var redraw = true;
+var moveSize = 25;
+for (int x = -width; x < width; x++)
+    DrawColumn(world, x + posX, x + width);
 do
 {
-    if (redraw)
-        for (int x = -width; x < width; x++)
-            DrawColumn(world, x + posX, x + width);
-    redraw = false;
-
     switch (Console.ReadKey().Key)
     {
         case ConsoleKey.RightArrow:
-            posX++;
-            redraw = true;
+            posX += moveSize;
+            Console.MoveBufferArea(moveSize, 0, width * 2 - moveSize, Console.WindowHeight, 0, 0);
+            for (int x = width - moveSize; x < width; x++)
+                DrawColumn(world, x + posX, x + width);
             break;
         case ConsoleKey.LeftArrow:
-            posX--;
-            redraw = true;
+            posX -= moveSize;
+            Console.MoveBufferArea(0, 0, width * 2 - moveSize, Console.WindowHeight, moveSize, 0);
+            for (int x = -width; x < -width + moveSize; x++)
+                DrawColumn(world, x + posX, x + width);
             break;
     }
 } while (true);
@@ -35,12 +36,12 @@ static void DrawColumn(World world, int x, int screenX)
 
 static void DrawBlock(World world, int x, int y, int screenX)
 {
-    (Console.ForegroundColor, var symbol) = world[x, y] switch
+    (Console.ForegroundColor, Console.BackgroundColor, var symbol) = world[x, y] switch
     {
-        Block.Bedrock => (ConsoleColor.Black, '#'),
-        Block.Stone => (ConsoleColor.Gray, '#'),
-        Block.Water => (ConsoleColor.Blue, '~'),
-        Block.Air => (ConsoleColor.White, ' '),
+        Block.Bedrock => (ConsoleColor.Black, ConsoleColor.Black, '#'),
+        Block.Stone => (ConsoleColor.Gray, ConsoleColor.Gray, '#'),
+        Block.Water => (ConsoleColor.Blue, ConsoleColor.DarkBlue, '~'),
+        Block.Air => (ConsoleColor.White, ConsoleColor.Black, ' '),
     };
     Console.SetCursorPosition(screenX, world.Height - y);
     Console.Write(symbol);
