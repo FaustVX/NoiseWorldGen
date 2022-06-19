e5d816348559b5ee251ef2feb67b0e6ce8292c52
FaustVX <jodu035@gmail.com>
2022-06-19 04:38:04 +0200

    Draw `TreeCutter`'s `ItemStack`s Quantity

--- NoiseWorldGen.Wpf/Tiles/Windows/TreeCutter.cs
+++ NoiseWorldGen.Wpf/Tiles/Windows/TreeCutter.cs
@@ -25,48 +25,67 @@ public class TreeCutter : TickedFeatureTileWindow
         public override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            _sb.Draw(_sb.Pixel(), new Rectangle(new(5), new(500)), Color.Black);
+            _sb.DrawLine(new Point(5, 0), new(5, GraphicsDevice.Viewport.Height), 5, Color.Green);
+            _sb.DrawLine(new Vector2(5, GraphicsDevice.Viewport.Height), new(5, GraphicsDevice.Viewport.Height - (float)GraphicsDevice.Viewport.Height / ItemStack.Max * Stack.Quantity), Color.Red);
         }
     }
     public new Tiles.TreeCutter Tile => (Tiles.TreeCutter)base.Tile;
-    protected readonly MonoGameControls.MonoGameContentControl _item;
-    private readonly ItemStackVM _itemVM;
-    private readonly Panel _space;
+    protected readonly MonoGameControls.MonoGameContentControl _sappling, _wood;
+    private readonly ItemStackVM _sapplingVM, _woodVM;
+
     public TreeCutter(Tiles.TreeCutter tile)
         : base(tile)
     {
-        _item = new()
+        _wood = new()
         {
             Window = this,
         };
+        _sappling = new()
+        {
+            Window = this,
+        };
+
         Content = null;
         Content = new Grid()
         {
-            Children =
+            RowDefinitions =
             {
-                _item,
-                new Grid()
+                new()
                 {
-                    RowDefinitions =
-                    {
-                        new()
-                        {
-                            Height = System.Windows.GridLength.Auto,
-                        },
-                        new()
-                        {
-                            Height = new(1, System.Windows.GridUnitType.Star),
-                        },
-                    },
-                    Children =
-                    {
-                        _panel,
-                        (_space = new Grid()),
-                    },
+                    Height = System.Windows.GridLength.Auto,
+                },
+                new()
+                {
+                    Height = new(1, System.Windows.GridUnitType.Star),
                 },
             },
+            ColumnDefinitions =
+            {
+                new()
+                {
+                    Width = new(1, System.Windows.GridUnitType.Star),
+                },
+                new()
+                {
+                    Width = new(1, System.Windows.GridUnitType.Star),
+                },
+            },
+            Children =
+            {
+                _panel,
+                _wood,
+                _sappling,
+            },
         };
-        _space.SetValue(Grid.RowProperty, 1);
-        _item.DataContext = _itemVM = new(Tile.SapplingStored);
+
+        _panel.SetValue(Grid.ColumnSpanProperty, 2);
+
+        _wood.SetValue(Grid.RowProperty, 1);
+        _wood.SetValue(Grid.ColumnProperty, 0);
+        _wood.DataContext = _woodVM = new(Tile.WoodStored);
+
+        _sappling.SetValue(Grid.RowProperty, 1);
+        _sappling.SetValue(Grid.ColumnProperty, 1);
+        _sappling.DataContext = _sapplingVM = new(Tile.SapplingStored);
     }
 }
